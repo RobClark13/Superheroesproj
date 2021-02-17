53e7cc2888fdff5c3631f10d506dfb8957b1f116
Rob Clark <clarkrob13@gmail.com>
2021-02-17 13:47:43 -0500

    Added methods to Controller for Index and Create and Added views for Index and Create

---
+++ Superheroes/Controllers/Superhero.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Superheroes.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Superheroes.Models;
+
+namespace Superheroes.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public SuperheroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var superheroes = _context.Superheroes;
+            return View(superheroes);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Superhero superhero)
+        {
+            _context.Superheroes.Add(superhero);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(int id)
+        {
+            var superheroDetails = _context.Superheroes.Find(id);
+            return View(superheroDetails);
+        }
+
+      
+        
+    }
+}
--- Superheroes/Data/Migrations/20210217173710_Init.Designer.cs
+++ Superheroes/Data/Migrations/20210217173710_Init.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Superheroes.Data;
 
 namespace Superheroes.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210217173710_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -221,18 +223,13 @@ namespace Superheroes.Data.Migrations
 
             modelBuilder.Entity("Superheroes.Models.Superhero", b =>
                 {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+                    b.Property<string>("Name")
+                        .HasColumnType("nvarchar(450)");
 
                     b.Property<string>("AlterEgo")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Catchphrase")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("Name")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("PrimaryAbility")
@@ -241,7 +238,7 @@ namespace Superheroes.Data.Migrations
                     b.Property<string>("SecondaryAbility")
                         .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("Id");
+                    b.HasKey("Name");
 
                     b.ToTable("Superheroes");
                 });
